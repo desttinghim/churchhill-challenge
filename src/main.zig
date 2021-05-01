29225298927ef2f5a6f402adca9c326ae7678ac4
Louis Pearson <louispearson@librem.one>
2021-04-30 21:49:33 -0600

    Clean up

--- src/main.zig
+++ src/main.zig
@@ -31,19 +31,10 @@ const SearchContext = struct {
 
 export fn create(points_begin: [*]Point, points_end: *Point) callconv(.C) *SearchContext {
     gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    // var raw_len = @ptrToInt(points_end) - @ptrToInt(points_begin);
-    // std.log.debug("raw_len {}", .{raw_len});
-    // std.log.debug("Point {}", .{@sizeOf(Point)});
     var len = (@ptrToInt(points_end) - @ptrToInt(points_begin)) / @sizeOf(Point);
-    // std.log.debug("\nlen {}", .{len});
-    // var points = gpa.allocator.dupe(Point, points_begin[0..len]) catch |e| @panic("Out of memory!");
     var sc = gpa.allocator.create(SearchContext) catch |e| @panic("Out of memory!");
 
     sc.points = gpa.allocator.dupe(Point, points_begin[0..len]) catch |e| @panic("Out of memory!");
-
-    // for (sc.points) |point| {
-    //     point.print();
-    // }
 
     return sc;
 }
@@ -60,26 +51,16 @@ fn point_is_inside(p: Point, rect: Rect) bool {
 }
 
 export fn search(sc: *SearchContext, rect: *const Rect, count: i32, out_points: [*]Point) callconv(.C) i32 {
-    // std.log.debug("\n", .{});
-    // rect.print();
     var pointlist = std.ArrayList(Point).init(&gpa.allocator);
     defer pointlist.deinit();
     for (sc.points) |point| {
         if (point_is_inside(point, rect.*)) {
             pointlist.append(point) catch |e| @panic("Out of memory!");
         }
-        // else {
-        //     std.log.debug("not in bounds:", .{});
-        //     rect.print();
-        //     point.print();
-        // }
     }
     std.sort.sort(Point, pointlist.items, @as(u0, 0), point_cmp);
     const mincount = std.math.min(@intCast(usize, count), pointlist.items.len);
     std.mem.copy(Point, out_points[0..mincount], pointlist.items[0..mincount]);
-    // for (pointlist.items[0..mincount]) |point, i| {
-    //     std.log.debug("{}: {}", .{ i, point });
-    // }
     return @intCast(i32, mincount);
 }
 
