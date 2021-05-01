63f215ab18c7dafdb9869e346c6777ddbbbbadef
Louis Pearson <louispearson@librem.one>
2021-04-30 22:46:01 -0600

    Add test

--- src/main.zig
+++ src/main.zig
@@ -1,25 +1,25 @@
 const std = @import("std");
 const testing = std.testing;
 
-const Point = packed struct {
+pub const Point = packed struct {
     id: i8,
     rank: i32,
     x: f32,
     y: f32,
 
     fn print(self: @This()) void {
-        std.log.debug("id: {}, rank: {}, x: {d}, y: {d}", .{ self.id, self.rank, self.x, self.y });
+        std.log.warn("id: {}, rank: {}, x: {d}, y: {d}", .{ self.id, self.rank, self.x, self.y });
     }
 };
 
-const Rect = packed struct {
+pub const Rect = packed struct {
     lx: f32,
     ly: f32,
     hx: f32,
     hy: f32,
 
     fn print(self: @This()) void {
-        std.log.debug("lx: {d}, ly: {d}, hx: {d}, hy: {d}", .{ self.lx, self.ly, self.hx, self.hy });
+        std.log.warn("lx: {d}, ly: {d}, hx: {d}, hy: {d}", .{ self.lx, self.ly, self.hx, self.hy });
     }
 };
 
@@ -29,12 +29,16 @@ const SearchContext = struct {
     points: []Point,
 };
 
-export fn create(points_begin: [*]Point, points_end: *Point) callconv(.C) *SearchContext {
+export fn create(points_begin: [*]const Point, points_end: *const Point) callconv(.C) *SearchContext {
     gpa = std.heap.GeneralPurposeAllocator(.{}){};
     var len = (@ptrToInt(points_end) - @ptrToInt(points_begin)) / @sizeOf(Point);
     var sc = gpa.allocator.create(SearchContext) catch |e| @panic("Out of memory!");
 
     sc.points = gpa.allocator.dupe(Point, points_begin[0..len]) catch |e| @panic("Out of memory!");
+
+    for (sc.points) |point| {
+        std.log.warn("point {}", .{point});
+    }
 
     return sc;
 }
@@ -51,6 +55,7 @@ fn point_is_inside(p: Point, rect: Rect) bool {
 }
 
 export fn search(sc: *SearchContext, rect: *const Rect, count: i32, out_points: [*]Point) callconv(.C) i32 {
+    std.log.debug("rect {}", .{rect});
     var pointlist = std.ArrayList(Point).init(&gpa.allocator);
     defer pointlist.deinit();
     for (sc.points) |point| {
@@ -61,6 +66,11 @@ export fn search(sc: *SearchContext, rect: *const Rect, count: i32, out_points: 
     std.sort.sort(Point, pointlist.items, @as(u0, 0), point_cmp);
     const mincount = std.math.min(@intCast(usize, count), pointlist.items.len);
     std.mem.copy(Point, out_points[0..mincount], pointlist.items[0..mincount]);
+    std.log.warn("count {}", .{count});
+    std.log.warn("mincount {}", .{mincount});
+    for (out_points[0..mincount]) |out_point| {
+        std.log.warn("out_point {}", .{out_point});
+    }
     return @intCast(i32, mincount);
 }
 
@@ -69,4 +79,23 @@ export fn destroy(sc: *SearchContext) callconv(.C) ?*SearchContext {
     gpa.allocator.destroy(sc);
     _ = gpa.deinit();
     return null;
+}
+
+test "test" {
+    const test_points = @import("test_data.zig").test_points;
+    const tests = @import("test_data.zig").tests;
+
+    var sc = create(&test_points, @intToPtr(*const Point, @ptrToInt(&test_points) + test_points.len * @sizeOf(Point)));
+
+    for (tests) |search_test, i| {
+        var buf = try std.testing.allocator.alloc(Point, search_test.count);
+        defer std.testing.allocator.free(buf);
+        var test_count = search(sc, &search_test.rect, @intCast(i32, search_test.count), buf.ptr);
+        std.testing.expectEqual(@intCast(i32, search_test.actual), test_count);
+        std.testing.expectEqualSlices(Point, search_test.items, buf[0..@intCast(usize, test_count)]);
+    }
+
+    var res = destroy(sc);
+
+    std.testing.expectEqual(@as(?*SearchContext, null), res);
 }
