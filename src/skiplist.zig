e3e94ef0a5a07e6d53aed579531608c6d8eaee11
Louis Pearson <louispearson@librem.one>
2021-05-21 02:11:03 -0600

    Lookup is working

--- src/skiplist.zig
+++ src/skiplist.zig
@@ -112,7 +112,8 @@ pub fn CSSL(comptime Key: type, comptime Value: type, cmp: CompareFns(Key), comp
             var lvl = levels - 1;
             while (lvl > 0) : (lvl -= 1) {
                 // var rPos = pos - level_start_pos[level];
-                while (cmp.isLessThan(key, self.fastLanes[lvl].items[pos])){
+                // std.log.warn("pos {}, lvl {}, len {}", .{pos, lvl, self.fastLanes[lvl].items.len});
+                while (pos < self.fastLanes[lvl].items.len and cmp.isLessThan(key, self.fastLanes[lvl].items[pos])){
                     // rPos += 1;
                     pos += 1;
                 }
@@ -145,6 +146,7 @@ fn u8isGreaterThan(a: u8, b: u8) bool {
 
 test "Cache Sensitive Skip List" {
     const ordered_list_keys: [10]u8 = .{0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    const ordered_list_data: [10]u8 = .{'e', 'd', 'c', 'b', 'a', 'j', 'i', 'h', 'g', 'f'};
     const list_keys: [10]u8 = .{4, 3, 2, 1, 0, 9, 8, 7, 6, 5};
     const list_data: [10]u8 = .{'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'};
     const cmp = CompareFns(u8) {
@@ -171,225 +173,12 @@ test "Cache Sensitive Skip List" {
 
     if (cssl.lookup(6)) |val| {
         try std.testing.expectEqual(val, 'i');
+        std.log.warn("val {c}", .{val});
     } else {
         return error.TestFailed;
     }
+    if (cssl.lookup(11)) |val| {
+        std.log.warn("val {c}", .{val});
+        return error.TestPassedWrong;
+    }
 }
-
-// fn SkipList(comptime T: type, cmp: fn (T, T) bool) type {
-//     return struct {
-//         allocator: *std.mem.Allocator,
-//         max_level: u8,
-//         skip: u8,
-//         num_elements: usize,
-//         head: *DataNode(T),
-//         tail: *DataNode(T),
-//         items_per_level: []u32,
-//         fast_lane_items: []u32,
-//         start_of_fast_lanes: []u32,
-//         fast_lanes: []T,
-//         fast_lane_pointers: []?[]ProxyNode(T),
-
-//         fn init(allocator: *std.mem.Allocator, maxLevel: u8, skip: ?u8) @This() {
-//             var self = @This(){
-//                 .allocator = allocator,
-//                 // .max_level = maxLevel,
-//                 .num_elements = 0,
-//                 .data = std.ArrayList(DataNode).init(allocator),
-//                 .skip = skip orelse 2,
-//                 .items_per_level = allocator.alloc(u32, maxLevel),
-//                 .fast_lane_items = allocator.alloc(u32, maxLevel),
-//                 .start_of_fast_lanes = allocator.alloc(u32, maxLevel),
-//                 .fast_lanes = undefined,
-//                 .fast_lane_pointers = undefined,
-//             };
-//             var i: usize = 0;
-//             while (i < maxLevel) : (i += 1) {
-//                 self.fast_lane_items[i] = 0;
-//             }
-
-//             self.buildFastLanes();
-
-//             return self;
-//         }
-//         // Figure out how to build. Specifically figure out how to make the fast lanes the correct size
-//         // fn buildFromSlice(allocator: *std.mem.Allocator, maxLevel: u8, skip: ?u8, items: []T) void {
-//         //     var self = @This(){
-//         //         .allocator = allocator,
-//         //         .max_level = maxLevel,
-//         //         .num_elements = 0,
-//         //         .data = std.ArrayList(DataNode).initCapacity(allocator, items.len),
-//         //         .skip = skip orelse 2,
-//         //         .items_per_level = allocator.alloc(u32, maxLevel),
-//         //         .fast_lane_items = allocator.alloc(u32, maxLevel),
-//         //         .start_of_fast_lanes = allocator.alloc(u32, maxLevel),
-//         //         .fast_lanes = undefined,
-//         //         .fast_lane_pointers = undefined,
-//         //     };
-//         //     var i: usize = 0;
-//         //     while (i < maxLevel) : (i += 1) {
-//         //         self.fast_lane_items[i] = 0;
-//         //     }
-
-//         //     self.buildFastLanes();
-
-//         //     return self;
-//         // }
-//         fn deinit(self: *@This()) void {
-//             self.allocator.free(self.items_per_level);
-//             self.allocator.free(self.start_of_fast_lanes);
-//             self.allocator.free(self.fast_lane_items);
-//             self.data.deinit();
-//         }
-//         fn insert(self: *@This(), key: u32) !void {
-//             var new_node = try self.allocator.create(DataNode(T));
-//             var nodeInserted = true;
-//             var fastLaneInserted = false;
-
-//             self.tail.next = new_node;
-//             self.tail = new_node;
-
-//             var level: usize = 0;
-//             while (level < self.max_level) : (level += 1) {
-//                 if (self.num_elements % std.math.pow(self.skip, level + 1) == 0 and nodeInserted) {
-//                     nodeInserted = try self.insertItemIntoFastLane(level, new_node);
-//                 } else {
-//                     break;
-//                 }
-//                 fastLaneInserted = true;
-//             }
-
-//             if (!fastLaneInserted)
-//                 try self.findAndInsertProxyNode(new_node);
-
-//             self.num_elements += 1;
-
-//             if (self.num_elements % (TOP_LANE_BLOCK * std.math.pow(self.skip, self.max_level)) == 0)
-//                 try self.resizeFastLanes();
-//         }
-//         fn insertItemIntoFastLane(self: *@This(), level: u8, newNode: *DataNode) !?u32 {
-//             var curPos = self.starts_of_fast_lanes[level] + self.fast_lane_items[level];
-//             var levelLimit = curPos + self.items_per_level[level];
-
-//             if (curPos > levelLimit)
-//                 curPos = levelLimit;
-
-//             while (newNode.key > self.fast_lanes[curPos] and curPos < levelLimit)
-//                 curPos += 1;
-
-//             if (self.fast_lanes[curPos] == null) {
-//                 sefl.fast_lanes[curPos] = newNode.key;
-//                 if (level == 0) {
-//                     self.fast_lane_pointers[curPos - self.starts_of_fast_lanes[0]] = self.newProxyNode(node);
-//                 }
-//                 self.fast_lane_items[level] += 1;
-//             } else {
-//                 return null;
-//             }
-
-//             return curPos;
-//         }
-//         fn newProxyNode(self: *@This(), node: *DataNode) *ProxyNode {
-//             var newProxy = self.allocator.create(ProxyNode);
-//             newProxy.keys[0] = node.key;
-//             newProxy.updated = false;
-//             var i: u8 = 1;
-//             while (i < self.skip) : (i += 1) {
-//                 newProxy.keys[i] = null;
-//             }
-//             newProxy.pointers[0] = node;
-
-//             return newProxy;
-//         }
-//         fn findAndInsertIntoProxyNode(self: *@This(), node: *DataNode) void {
-//             var proxy = self.fast_lane_pointers[self.fast_lane_items[0] - 1];
-//             var i: u8 = 1;
-//             while (i < self.skip) : (i += 1) {
-//                 if (proxy.keys[i] == null) {
-//                     proxy.keys[i] == node.key;
-//                     proxy.pointers[i] = node;
-//                     return;
-//                 }
-//             }
-//         }
-//         fn buildFastLanes(self: *@This()) void {
-//             var fast_lane_size = TOP_LANE_BLOCK;
-
-//             self.items_per_level[self.max_level - 1] = fast_lane_size;
-//             self.start_of_fast_lanes[self.max_level - 1] = 0;
-
-//             var level: u8 = self.max_level - 2;
-//             while (level >= 0) : (level -= 1) {
-//                 self.items_per_level[level] = self.items_per_level[level + 1] * self.skip;
-//                 self.start_of_fast_lanes[level] = self.start_of_fast_lanes[level + 1] + self.items_per_level[level + 1];
-//                 fast_lane_size += self.items_per_level[level];
-//             }
-
-//             self.fast_lanes = self.allocator.alloc(u32, fast_lane_size);
-//             self.fast_lane_pointers = self.allocator.alloc(*ProxyNode, self.items_per_level[0]);
-
-//             // Placeholder values
-//             for (self.fast_lanes) |_, i| {
-//                 self.fast_lanes[i] = std.math.maxInt(u32);
-//             }
-//             for (self.fast_lane_pointers) |_, i| {
-//                 self.fast_lane_pointers[i] = null;
-//             }
-//         }
-//         fn resizeFastLanes(self: *@This()) void {
-//             var new_size = self.
-//         }
-//         fn searchElement(self: *@This(), key: u32) ?u32 {
-//             var curPos: usize = 0;
-//             var first: usize = 0;
-//             var last: usize = self.items_per_level[self.max_level - 1] - 1;
-//             var middle: usize = 0;
-
-//             // scan highest fast lane with binary search
-//             while (first < last) {
-//                 middle = (first + last) / 2;
-//                 if (self.fast_lanes[middle] < key) {
-//                     first = middle + 1;
-//                 } else if (self.fast_lanes[middle] == key) {
-//                     curPos = middle;
-//                     break;
-//                 } else {
-//                     last = middle;
-//                 }
-//             }
-
-//             // if first is greater than last, it was an incrementing error, use last instead
-//             if (first > last) curPos = last;
-
-//             // traverse fast lanes
-//             var level: usize = self.max_level - 1;
-//             while (level >= 0) : (level -= 1) {
-//                 var rPos = curPos - self.start_of_fast_lanes[level];
-//                 curPos += 1;
-//                 // linear search through fast lane while key is gt/eq
-//                 while (rPos < self.items_per_level[level] and key >= self.fast_lanes[curPos]) {
-//                     rPos += 1;
-//                 }
-//                 // if we are at the lowest fast lane, go to next step
-//                 if (level == 0) break;
-//                 // otherwise, go down a level, multiplying by skip amount
-//                 curPos = self.start_of_fast_lanes[level - 1] + rPos * self.skip;
-//             }
-
-//             // algorithm overshoots, bring it down by one
-//             curPos -= 1;
-//             //
-//             if (key == self.fast_lanes[curPos]) return key;
-
-//             var proxy = self.fast_lane_pointers[curPos - @ptrToInt(self.start_of_fast_lanes[0])];
-//             var i: usize = 1;
-//             while (i < self.skip) : (i += 1) {
-//                 if (proxy.keys[i] == key)
-//                     return key;
-//             }
-
-//             return null;
-//         }
-//         fn searchRange(self: *@This(), startKey: u32, endKey: u32) []DataNode {}
-//     };
-// }
